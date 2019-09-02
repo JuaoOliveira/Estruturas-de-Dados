8ce6ecc06ef5ade17f6bdda76b41129c2741f406
JuaoOliveira <jvoliveira3011@gmail.com>
2019-09-02 19:04:57 +0000

    Código da torre de hanói funcional

--- source/hanoi.cpp
+++ source/hanoi.cpp
@@ -3,14 +3,22 @@
 
 using namespace std;
 
-void hanoi(int num, int *origem, int *trab, int *dest){
+void hanoi(int num, char origem, char destino, char trabalho){
     if(num > 0){
-        hanoi(num-1, origem, dest, trab);
-        
+        hanoi(num-1, origem, trabalho, destino);
+        cout << origem << " -> " << destino << endl;
+        hanoi(num-1, trabalho, destino, origem);
     }
 
 }
 
 int main(){
+    int numDiscos;
+
+    cout << "Digite o numero de discos: ";
+    cin >> numDiscos;
+
+    hanoi(numDiscos, 'A', 'B', 'C');
+
 
 }
