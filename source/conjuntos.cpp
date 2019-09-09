b333d783a468b873bcaf39c95489d12c6e9a033b
JuaoOliveira <jvoliveira3011@gmail.com>
2019-09-09 21:11:44 +0000

    Atualização do trabalho 01

---
+++ source/conjuntos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "conjuntos.hpp"
+
+using namespace std;
+
+int main(){
+    Conjunto<int> c;
+
+    c.inicializar();
+
+    c.inserir(3);
+    c.inserir(666);
+    c.inserir(4);
+    c.inserir(12);
+    c.inserir(7);
+    c.remover(3);
+
+    for (int i = 0; i <= c.ult; i++)
+    {
+        cout << "elemento " << i << ": " << c.v[i] << endl;
+    }
+    
+}
---
+++ source/conjuntos.hpp
@@ -0,0 +1,67 @@
+#include <new>
+
+using std::nothrow;
+
+template <typename T>
+struct Conjunto{
+    T *v;
+    int tamVetor, ult;
+
+    bool inicializar(){
+        v = new(nothrow) T[1];
+
+        if(v == nullptr) return true;
+
+        tamVetor = 1;
+        ult = -1;
+
+        return false;
+    }
+
+    bool redimensionar(int novo_tam){
+        T *novo_v = new(nothrow) T[novo_tam];
+
+        if(novo_v == nullptr) return true;
+
+        for (int i = 0; i <= ult; ++i) novo_v[i] = v[i];
+
+        tamVetor = novo_tam;
+        delete[] v;
+        v = novo_v;
+
+        return false;
+    }
+
+    bool vazio(){
+        return ult == -1;
+    }
+
+    bool inserir(T e){
+        if(ult == tamVetor - 1) 
+            if(redimensionar(tamVetor*2)) return true;
+
+        v[++ult] = e;
+        return false;
+    }
+
+    bool pertence(T e){
+        for(int i = 0; i <= ult; ++i)
+            if(v[i] == e) return true;
+
+        return false;
+    }
+
+    bool remover(T e){
+        int i = 0;
+
+        while(v[i] != e) ++i;
+        
+        v[i] = v[ult];
+        --ult;
+        
+        if(!vazio() && 4*(ult+1) <= tamVetor)  
+            if(redimensionar(tamVetor/2)) return true;
+        
+        return false;
+    }
+};
